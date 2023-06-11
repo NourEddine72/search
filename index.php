4c2507a4e9c370240c1ea8b62fae5cb7bf58ee1e
ApkRoz <88466148+apkroz@users.noreply.github.com>
2023-06-11 20:35:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="ApkRoz.com-icon.webp">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  <?php
+  // Retrieve search query from URL parameter
+  $search_query = isset($_GET['q']) ? htmlspecialchars($_GET['q']) : '';
+
+  // Generate dynamic metadata based on search query
+  $title = $search_query ? "$search_query - ApkRoz" : "ApkRoz";
+  $description = $search_query ? "Find information about $search_query on ApkRoz." : "Welcome toApkRoz.";
+  $keywords = $search_query ? "$search_query, information, ApkRoz" : "ApkRoz, information, search";
+  ?>
+  
+  <title><?php echo $title ?></title>
+  <meta name="description" content="<?php echo $description ?>">
+  <meta name="keywords" content="<?php echo $keywords ?>">
+</head>
+<body>
+    <div class="container">
+      <img src="ApkRoz.com-title.webp" alt="Logo">
+  <h1>Find Information About : </h1>
+  <p><?php echo $description ?></p>
+  <p2>If you want to know more about this topic, click the button below</p2>
+      <button onclick="redirectTo('https://www.apkroz.com')"> Visit Website </button>
+ </div>
+    <script>
+      function redirectTo(url) {
+        window.location.href = url;
+      }
+    </script>
+    <div class="what-txt">
+    <h1>What is ApkRoz ?</h1>
+    </div>
+    <div class="post-details">
+  <div class="post-content">
+<p>Welcome to <a href="https://www.apkroz.com" style="color: #53c66d; font-weight: bold;">ApkRoz.com</a>, the ultimate hub for Android enthusiasts seeking an extensive collection of <strong>modded apps</strong>, <strong>games</strong>, and <strong>APK files</strong>. Explore our website and unlock a world of exciting possibilities for your <strong>Android devices</strong>.</p>
+
+<p>At ApkRoz.com, we understand the desire for unique and enhanced experiences on your Android device. Our platform offers a wide range of <strong>free downloads</strong> of the latest <strong>modded versions</strong> of popular apps and games, empowering you to enjoy <strong>premium features</strong>, <strong>unlimited resources</strong>, and <strong>exclusive content</strong>.</p>
+
+<p>Discover a treasure trove of diverse applications and games on <a href="https://www.apkroz.com" style="color: #53c66d; font-weight: bold;">ApkRoz.com</a>. From productivity tools, entertainment apps, to action-packed games, and immersive experiences, our platform caters to a wide range of interests and preferences. Browse through various <strong>categories</strong> and <strong>genres</strong> such as action, adventure, puzzle, strategy, and more, to find the perfect apps and games that resonate with your passion.</p>
+
+<p>Downloading your favorite apps and games is a breeze on <a href="https://www.apkroz.com" style="color: #53c66d; font-weight: bold;">ApkRoz.com</a>. Our user-friendly interface ensures a seamless experience, allowing you to <strong>download APK files</strong> with ease and convenience. With just a few clicks, you can access a vast library of exciting content and elevate your Android experience to new heights.</p>
+
+<p>Join our ever-growing community of Android enthusiasts who trust <a href="https://www.apkroz.com" style="color: #53c66d; font-weight: bold;">ApkRoz.com</a> as their go-to source for <strong>modded apps</strong> and <strong>games</strong>. Whether you're a gaming enthusiast, an app lover, or someone looking to enhance productivity, our platform caters to all your needs. Expand the horizons of your Android device and embark on an exhilarating journey with <a href="https://www.apkroz.com" style="color: #53c66d; font-weight: bold;">ApkRoz.com</a> today!</p>
+
+  </div>
+
+    <div class="publish-date">May 14, 2022</div>
+    </div>
+    <div class="about-txt"
+    <h1>About Publisher</h1>
+    </div>
+	<div class="profile-detail">    
+		<div class="head">
+			<div class="avat">
+				<img src="ApkRoz.com-logo.webp" alt="Profile Picture">
+			</div>
+			<div class="detai">
+				<h2>APK ROZ</h2>
+				<p>Last Update May 14, 2023</p>
+			</div>
+		</div>
+	</div>		
+</body>
+</html>
